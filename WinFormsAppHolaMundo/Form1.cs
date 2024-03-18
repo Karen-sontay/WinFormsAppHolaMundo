97b202de57e3e641225305b932148eb6d91722fe
ale <karensontay16@gmail.com>
2024-03-18 00:53:38 -0600

    Agregar archivos de proyecto.

---
+++ WinFormsAppHolaMundo/Form1.cs
@@ -0,0 +1,15 @@
+namespace WinFormsAppHolaMundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("HOLA MUNDO");
+        }
+    }
+}
